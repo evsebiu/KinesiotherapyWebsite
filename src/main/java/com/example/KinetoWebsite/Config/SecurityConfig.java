5bf3494d9ae0af6d65610b15f739b01c807ccdf0
evsebiu <evsebiu@gmail.com>
2026-01-08 22:57:48 +0200

    security /** update from permitAll. testing for render deployment

--- src/main/java/com/example/KinetoWebsite/Config/SecurityConfig.java
+++ src/main/java/com/example/KinetoWebsite/Config/SecurityConfig.java
@@ -28,7 +28,6 @@ public class SecurityConfig {
                                 "/js/**",
 
                                 "/api/appointments/**",
-                                "/**",
                                 "/gdpr/**",
                                 "/anpc/**",
 
